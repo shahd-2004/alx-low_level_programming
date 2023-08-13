efcf8c0b1e1b83aa4fc2a45d39dcb8b03e253603
shahd-2004 <shahdbaloula291@gmail.com>
2023-08-13 13:21:41 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,19 +19,22 @@
 int main(void)
 {
 int c;
-int d = 0;
-
+int d;
+int e = 0;
+while (e < 10)
+{
+d = 0;
 while (d < 10)
 {
 c = 0;
 while (c < 10)
 {
-if (d != c && d < c)
+if (c != d && d != e && e < d && d < c)
 {
+putchar('0' + e);
 putchar('0' + d);
 putchar('0' + c);
-
-if (c + d != 17)
+if (c + d + e != 9 + 8 + 7)
 {
 putchar(',');
 putchar(' ');
@@ -40,6 +43,8 @@ putchar(' ');
 c++;
 }
 d++;
+}
+e++;
 }
 putchar('\n');
 return (0);
